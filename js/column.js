83d1a0a674cc97ed26531e8ab8a759f190efe457
Joanna Piotrowska <PiotrowskaWIMIIP@gmail.com>
2018-05-19 02:26:31 +0200

    Add new function - change name Columns in js

--- js/column.js
+++ js/column.js
@@ -12,6 +12,7 @@ function Column(id, name) {
     var $columnCardList = $('<ul>').addClass('column-card-list');
     var $columnDelete = $('<button>').addClass('btn-delete').text('x');
     var $columnAddCard = $('<button>').addClass('add-card').text('Add a card');
+    var $columnChangeName = $('<button>').addClass('change-name').text('Change the name');
 
     // add events (click)
     $columnDelete.click(function() {
@@ -20,7 +21,7 @@ function Column(id, name) {
 
     $columnAddCard.click(function(event) {
       var cardName = prompt("Enter the name of the card");
-      event.preventDefault();    //????
+      event.preventDefault();    //????zatrzymuje domyslne działanie zdarzeń
       self.addCard(new Card(cardName));
 
       $.ajax({
@@ -37,11 +38,32 @@ function Column(id, name) {
       });
     });
 
+    $columnChangeName.click(function(event) {
+      var newNameColumn = prompt("Enter the new name of the column");
+      event.preventDefault();
+      self.changeNameColumn(self.$element);
+
+      $.ajax({
+        url: baseUrl + '/column/' + self.id,
+        method: 'PUT',
+        data: {
+        name: newNameColumn,
+        data: 'string'
+        bootcamp_kanban_column_id: self.id
+        },
+        success: function(response) {
+            var newName = self.$element.(self.id, newNameColumn);
+            self.changeNameColumn(newName);
+        }
+      });
+    })
+
     // construction column of element
     $column.append($columnTitle)
       .append($columnDelete)
       .append($columnAddCard)
-      .append($columnCardList);
+      .append($columnCardList)
+      .append($columnChangeName);
 
     // retur of created column
     return $column;
@@ -62,5 +84,11 @@ Column.prototype = {
         self.$element.remove(); //? dlaczego mam usunąc $??
       }
     });
+  },
+  changeNameColumn: function() {
+
+
+    $columnTitle.text(newNameColumn);
   }
+
 };
